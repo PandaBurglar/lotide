544f001de0e3e2bb41bc67a8dcf5d3f1ced19e92
PandaBurglar <zane.bokhari@gmail.com>
2021-11-22 20:19:33 -0500

    Implementing tail for lotide

---
+++ tail.js
@@ -0,0 +1,19 @@
+let tail = function(array) {
+  const newArray = array.splice[0];
+  return newArray;
+};
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
