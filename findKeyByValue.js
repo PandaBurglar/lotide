e70f9a7efc1ca682c0402c4f29f848e115460f60
PandaBurglar <zane.bokhari@gmail.com>
2021-11-25 16:56:20 -0500

    Scans an object & returns tthe first key which contains the given value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,25 +6,29 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama:  "The Wire",
+  horror:  "The Wire"
 };
-// which takes in an object and a value. 
-//It should scan the object and 
+// which takes in an object and a value.
+//It should scan the object and
 //return the first key which contains the given value
 // If no key with that given value is found, then it should return undefined.
 
 const findKeyByValue = (obj, value) => {
-  // use object.kyes to help search through all object keys
+  // use object.keys to help search through all object keys
   for (const key in obj) {
     if (obj[key] === value) {
-    return key;
+      return key;
     }
-  } 
+  }
   return undefined;
   // use for...of to loop over keys returned by Object.keys
 };
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
