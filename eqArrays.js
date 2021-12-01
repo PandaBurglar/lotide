fc0d6ede5f097d95374fd0c9c7c3a43f11a53fb6
PandaBurglar <zane.bokhari@gmail.com>
2021-12-01 00:07:33 -0500

    refactored using module.exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,14 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
 
 const eqArrays = (array1, array2) => {
   if(array1.length !== array2.length) {
     return false;
   }
-for (let i=0; i< array1.length; i++) {
+  for (let i=0; i< array1.length; i++) {
     if (array1[i] !== array2[i]) {
-    return false;                                              
+      return false;                                              
     }                                            
 } return true;
 };
 
-
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1, 4]), true);
+module.exports = eqArrays;
