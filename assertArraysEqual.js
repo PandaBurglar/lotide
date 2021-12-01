69de78e6d1c0064794266f328b96e25ce57c400b
PandaBurglar <zane.bokhari@gmail.com>
2021-12-01 00:16:35 -0500

    refactored using module.exports & require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,17 @@
-const assertArrayEqual = (array1,array2) => {
-console.log(eqArrays(array1, array2) === true ? '✅ TRUE' : '🛑 FALSE' );
+const assertEqual = require('../lotide/assertEqual');
+const eqArrays = require('../lotide/eqArrays');
+                                  
+const assertArrayEqual = (array1,array2) => { // create a function that takes two arrays and console.logs a statement
+  console.log(eqArrays(array1, array2) === true ? '✅ TRUE' : '🛑 FALSE'); // if eq of the two arrays is also true, print true or else false
 };
 
-
-
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-for (let i=0; i< array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-    return false;                                              
-    }                                            
-} return true;
-};                                                    
-
-// FUNCTION IMPLEMENTATION
-/* const assertEqual = function(actual, expected) {
-  if (actual === expected) {                           
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-}; 
+module.exports = assertArrayEqual;
+  
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1, 4]), true); */
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1, 4]), true); 
                                                       
-assertArrayEqual([1, 2, 3], [1, 2, 3]);                                      
+assertArrayEqual([1, 2, 3], [1, 2, 3]);                                  
                                                                                                                                                                                            
